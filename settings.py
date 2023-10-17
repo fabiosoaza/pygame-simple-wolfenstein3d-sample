4a813f8eac67095eff8d1719f6fc2d5ab9aa4a52
Fábio Souza <fabio@solaire>
2023-10-17 05:09:26 -0300

    Create basic mini map with basic controls and game world collision

---
+++ settings.py
@@ -0,0 +1,10 @@
+import math
+RES = WIDTH, HEIGHT = 1600, 900
+
+FPS = 60
+PLAYER_POS = 1.5, 5  # mini_map
+PLAYER_ANGLE = 0
+#move speed
+PLAYER_SPEED = 0.004
+#rotation speed
+PLAYER_ROT_SPEED = 0.002
