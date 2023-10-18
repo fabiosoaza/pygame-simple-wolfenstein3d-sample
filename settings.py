970d9c1b1e0a9a1bde2ac26588f72289b7e17008
Fábio Souza <fabio@solaire>
2023-10-18 01:08:07 -0300

    Adding RayCasting

--- settings.py
+++ settings.py
@@ -1,10 +1,20 @@
 import math
+
 RES = WIDTH, HEIGHT = 1600, 900
 
 FPS = 60
 PLAYER_POS = 1.5, 5  # mini_map
 PLAYER_ANGLE = 0
-#move speed
+# move speed
 PLAYER_SPEED = 0.004
-#rotation speed
+# rotation speed
 PLAYER_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+# angle between the rays
+DELTA_ANGLE = FOV / NUM_RAYS
+# draw distance
+MAX_DEPTH = 20
