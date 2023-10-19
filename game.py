69541ff299b017f131d4e4e494b7fbbac5ff0f41
Fábio Souza <fabio@solaire>
2023-10-19 04:39:58 -0300

    Added textures and sprite support

--- game.py
+++ game.py
@@ -1,34 +1,46 @@
 import pygame as pg
 import sys
 from settings import *
+from levels import *
 from map import *
 from player import *
-from raycating import *
+from raycasting import *
+from object_renderer import *
+
+from object_handler import *
+
 
 
 class Game:
     def __init__(self):
         pg.init()
+        pg.mouse.set_visible(MOUSE_VISIBLE)
+
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
         self.new_game()
 
     def new_game(self):
-        self.map = Map(self)
+        self.map = Map(self, MINI_MAP)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
-
+        self.static_sprite = SpriteObject(self)
+        self.animated_sprite = AnimatedSprite(self)
+        self.object_handler = ObjectHandler(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.object_handler.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() : .1f}')
 
     def draw(self):
         self.screen.fill('black')
+        self.object_renderer.draw()
         # self.map.draw()
         # self.player.draw()
 
--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ RES = WIDTH, HEIGHT = 1600, 900
 HALF_WIDTH = WIDTH //2
 HALF_HEIGHT = HEIGHT // 2
 
+# DOOM original FPS 35
 FPS = 60
 PLAYER_POS = 1.5, 5  # mini_map
 PLAYER_ANGLE = 0
@@ -12,14 +13,34 @@ PLAYER_SPEED = 0.004
 # rotation speed
 PLAYER_ROT_SPEED = 0.002
 
+PLAYER_SIZE_SCALE = 60
+
+MOUSE_VISIBLE = False
+MOUSE_ENABLED = False
+
+MOUSE_SENSITIVITY = 0.0003
+MOUSE_MAX_REL = 40
+MOUSE_BORDER_LEFT = 100
+MOUSE_BORDER_RIGHT = WIDTH - MOUSE_BORDER_LEFT
+
+FLOOR_COLOR = (30, 30, 30)
+
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
 NUM_RAYS = WIDTH // 2
 HALF_NUM_RAYS = NUM_RAYS // 2
 # angle between the rays
 DELTA_ANGLE = FOV / NUM_RAYS
-# draw distance
+# max ray draw distance step
 MAX_DEPTH = 20
 
 SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
 SCALE = WIDTH // NUM_RAYS
+
+#texture resolution
+TEXTURE_SIZE = 256
+HALF_TEXTURE_SIZE = TEXTURE_SIZE //2
+
+TEXTURES_PATH='resources/textures'
+SPRITES_PATH='resources/sprites'
+
